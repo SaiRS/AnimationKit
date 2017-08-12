98a19e8d2807c4edb6a65eaedb2612bb6146603e
SaiJx <xjiaxiang86@163.com>
2017-08-12 17:19:10 +0800

    开始的测试index.js

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 // Only use require("babel-polyfill"); once in your whole app.
 // Multiple imports or requires of babel-polyfill will throw an error
 // since it can cause global collisions and other issues that are hard to trace.
@@ -6,8 +8,19 @@
 // https://github.com/babel/babel-preset-env
 import 'babel-polyfill';
 
+// import xxvJSDriver
+//   from '../../vendor/Action/ActionDriver/XXActionJSDriver.js';
+//
+
+import XXNodeDomActor from 'XXAction/Actor/XXNodeDomActor.js';
+import {XXPosition} from 'XXFundation/Type/XXPosition.js';
+import xxvLog from 'XXTool/LogTool.js';
+
 import Vue from 'vue';
 import hello from './js/hello.vue';
+
+
+require('./css/hello.css');
 
 new Vue({
   el: '.mount-point-for-vue-js',
@@ -16,3 +29,11 @@ new Vue({
     hello: hello,
   },
 });
+
+let nodeDomActor = new XXNodeDomActor('.animation-test-object1-js');
+nodeDomActor.position().printInfo();
+
+setTimeout(function() {
+  xxvLog.info('set to new position');
+  nodeDomActor.moveTo(new XXPosition(100, 500, 0));
+}, 5000);
