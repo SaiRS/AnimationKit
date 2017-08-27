9b16334a204014009139ba552c1b81cbcec4d835
SaiJx <xjiaxiang86@163.com>
2017-08-27 10:57:09 +0800

    delay action

---
+++ vendor/Action/Action/BaseAction/XXActionDelay.js
@@ -0,0 +1,33 @@
+// @flow
+//
+import XXActionInterval from '../XXActionInterval.js';
+
+/**
+ * 表示延迟的action
+ */
+class XXActionDelay extends XXActionInterval {
+
+  /**
+   * 构造函数
+   * @param  {number} delay 延迟的时间,单位ms
+   */
+  constructor(delay: number) {
+    super(delay);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  doDoneTask() {
+    // TODO: delay done task
+  }
+
+  /**
+   * @inheritdoc
+   */
+  className(): string {
+    return 'XXActionDelay';
+  }
+}
+
+export default XXActionDelay;
